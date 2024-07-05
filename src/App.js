6f66a11f87eca97eb0c9543a5ee3f5517f97f878
aparna162 <mohantyaparna41@gmail.com>
2024-07-05 17:18:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
